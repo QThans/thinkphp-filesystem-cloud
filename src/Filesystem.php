441e9e5b38227f4ae81c76523924eba96e740d02
thans <thans@thans.cn>
2020-01-19 03:22:56 +0800

    同步官方

--- src/Filesystem.php
+++ src/Filesystem.php
@@ -2,64 +2,55 @@
 
 namespace thans\filesystem;
 
-use think\App;
+use InvalidArgumentException;
 use think\filesystem\Driver;
 use think\helper\Arr;
+use think\Manager;
 
-class Filesystem
+class Filesystem extends Manager
 {
-    protected $disks;
-
-    /** @var App */
-    protected $app;
-
-    public function __construct(App $app)
-    {
-        $this->app = $app;
-    }
+    protected $namespace = '\\thans\\filesystem\\driver\\';
 
     /**
      * @param null|string $name
-     *
      * @return Driver
      */
     public function disk(string $name = null): Driver
     {
-        $name = $name ?: $this->app->config->get('filesystem.default');
+        return $this->driver($name);
+    }
 
-        if (!isset($this->disks[$name])) {
-            $config = $this->app->config->get("filesystem.disks.{$name}");
+    protected function resolveType(string $name)
+    {
+        return $this->getDiskConfig($name, 'type', 'local');
+    }
 
-            $this->disks[$name] = App::factory($config['type'], '\\thans\\filesystem\\driver\\', $config);
-        }
-
-        return $this->disks[$name];
+    protected function resolveConfig(string $name)
+    {
+        return $this->getDiskConfig($name);
     }
 
     /**
-     * 获取缓存配置.
-     *
+     * 获取缓存配置
+     * @access public
      * @param null|string $name    名称
      * @param mixed       $default 默认值
-     *
      * @return mixed
      */
     public function getConfig(string $name = null, $default = null)
     {
         if (!is_null($name)) {
-            return $this->app->config->get('filesystem.'.$name, $default);
+            return $this->app->config->get('filesystem.' . $name, $default);
         }
 
         return $this->app->config->get('filesystem');
     }
 
     /**
-     * 获取磁盘配置.
-     *
+     * 获取磁盘配置
      * @param string $disk
      * @param null   $name
      * @param null   $default
-     *
      * @return array
      */
     public function getDiskConfig($disk, $name = null, $default = null)
@@ -71,8 +62,23 @@ class Filesystem
         throw new InvalidArgumentException("Disk [$disk] not found.");
     }
 
+    /**
+     * 默认驱动
+     * @return string|null
+     */
+    public function getDefaultDriver()
+    {
+        return $this->getConfig('default');
+    }
+
+    /**
+     * 动态调用
+     * @param string $method
+     * @param array  $parameters
+     * @return mixed
+     */
     public function __call($method, $parameters)
     {
-        return $this->disk()->$method(...$parameters);
+        return $this->driver()->$method(...$parameters);
     }
 }
